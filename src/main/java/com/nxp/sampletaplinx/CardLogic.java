c6668f5b82d6a46ecfc278f5e46e1eb802ec8d6f
MowahhidShakeel <mowahhids@gmail.com>
2025-09-18 18:41:03 +0500

    Restored to original offset

--- src/main/java/com/nxp/sampletaplinx/CardLogic.java
+++ src/main/java/com/nxp/sampletaplinx/CardLogic.java
@@ -1079,11 +1079,11 @@ class CardLogic {
                 sdmSettings.setSDMEnabled(true);
                 sdmSettings.setUIDMirroringEnabled(true);
                 sdmSettings.setSDMReadCounterEnabled(true);
-                sdmSettings.setSdmAccessRights(new byte[]{(byte) 0x0E, 0x00, 0x00});
-                sdmSettings.setUidOffset(new byte[]{0x36, 0x00, 0x00});
-                sdmSettings.setSdmReadCounterOffset(new byte[]{0x51, 0x00, 0x00});
-                sdmSettings.setSdmMacOffset(new byte[]{0x67, 0x00, 0x00});
-                sdmSettings.setSdmMacInputOffset(new byte[]{0x31, 0x00, 0x00});
+                sdmSettings.setSdmAccessRights(new byte[]{(byte) 0xFE, (byte) 0xE1});
+                sdmSettings.setUidOffset(new byte[]{0x1A, 0x00, 0x00});
+                sdmSettings.setSdmReadCounterOffset(new byte[]{0x2D, 0x00, 0x00});
+                sdmSettings.setSdmMacOffset(new byte[]{0x39, 0x00, 0x00});
+                sdmSettings.setSdmMacInputOffset(new byte[]{0x39, 0x00, 0x00});
 
                 ntag424DNA.changeFileSettings(0x02, sdmSettings);
                 stringBuilder.append("SDM file settings configured (Encrypted mode, CMAC)\n");
